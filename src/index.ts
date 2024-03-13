602e52da13a124d7fa07a15cbf116ffe846d6b5f
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-03-13 08:25:58 -0300

    commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import diaryRouter from "./routes/diaries";
+
+const BACK_LOCAL_PORT: string | undefined = process.env.BACK_LOCAL_PORT;
+const BACK_URL_PORT: string | undefined = process.env.BACK_URL_PORT;
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = BACK_LOCAL_PORT || BACK_URL_PORT;
+
+app.get("/ping", (_req, res) => {
+  console.log("Someone pinged here");
+  res.send("pong");
+});
+
+app.use("/api/diaries", diaryRouter);
+
+app.listen(PORT, () => {
+  console.log("Server running on port:", PORT);
+});
